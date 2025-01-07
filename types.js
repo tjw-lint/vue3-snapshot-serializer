c77b03d5038b5576aa3d726ef0ac7007d9933d25
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2025-01-07 10:27:25 -0500

    Update types.js

--- types.js
+++ types.js
@@ -52,12 +52,12 @@
 /** @typedef {'root'|'tag'|'text'|'comment'|'doctype'|'cdata'|'script'|'style'|'directive'} ASTNODETYPE */
 
 /**
- * @typedef  {object}       ASTNODE
- * @property {ASTNODETYPE}  type       The node type
- * @property {string}       [name]     Any tag name ('h1', 'div', 'svg', 'my-component-stub')
- * @property {ASTNODE[]}    children   Array of nested AST nodes
- * @property {string}       data       The value of the node, if it is a comment or text
- * @property {object}       [attribs]  The attributes if the node is a tag, ex: { class: 'a b', title: 'c' }
+ * @typedef  {object}      ASTNODE
+ * @property {ASTNODETYPE} type       The node type
+ * @property {string}      [name]     Any tag name ('h1', 'div', 'svg', 'my-component-stub')
+ * @property {ASTNODE[]}   children   Array of nested AST nodes
+ * @property {string}      data       The value of the node, if it is a comment or text
+ * @property {object}      [attribs]  The attributes if the node is a tag, ex: { class: 'a b', title: 'c' }
  */
 
 export const types = {};
